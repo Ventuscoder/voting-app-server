dbd3459b49e5495327656fd2273435ba8bf3cef4
ventuscoder <shivankcode@gmail.com>
2023-03-11 17:30:12 +0530

    Connect application to mongoose

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
+
+let uri = `mongodb+srv://voting-app:${process.env.PASSWORD_DB}@cluster0.bdehkf7.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(uri)
+
+const pollSchema = mongoose.Schema({
+    topic: String,
+    options: [{opt: String, votes: Number}]
+})
+
+const Polls = mongoose.model('Poll', pollSchema)
 
 const app = express()
 

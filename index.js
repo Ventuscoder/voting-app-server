f443cf8ab7e4e84a9f9288d45c08213634c0af15
ventuscoder <shivankcode@gmail.com>
2023-03-17 17:12:33 +0530

    Add update functionality

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get('/enter/:id', (req, res)=>{
     polls.findOne({_id: req.params.id}).then(data=>res.send(data)).catch(err=>res.send('Data not found'))
 })
 
+app.get('/update/:poll/:option', (req, res)=>{
+    polls.findOne({_id: req.params.poll}).then(data=>{
+        let newOptions = data.options.map(option=>option._id==req.params.option ? {opt: option.opt, votes: option.votes+1, _id: option._id} : option)
+        polls.findOneAndUpdate({_id: req.params.poll}, {topic: data.topic, options: newOptions}).then(res.send('Updated!')).catch(err=>res.send('Could not update'))
+    })
+    /* polls.findOne({_id: req.params.poll}).then(data=>{
+        let newOptions = data.options.map(option=>option._id==req.params.option ? {opt: option.opt, votes: option.votes+1} : option)
+        polls.findOneAndUpdate({_id: req.params.poll}, {topic: data.topic, newOptions}).then(re.send('Updated!')).catch(err=>res.send('Could not update'))
+    }).catch(err=>res.send('Data not found')) */
+})
+
 app.listen(8000, ()=>{
     console.log('Server is running on port 8000')
 })
